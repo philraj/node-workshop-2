4d8f9c3598e5d97cc8d9d391662ee9e153915677
Phil Rajchgot <tophil@outlook.com>
2016-02-02 20:23:07 +0000

    Adding library/fortune.js  module

---
+++ library/fortune.js
@@ -0,0 +1,10 @@
+var fortunes = [
+    "You will be very rich.",
+    "You will be very poor.",
+    "You will become very powerful.",
+    "You will become weak and powerless."
+]
+
+function getFortune () {
+    return fortunes[ Math.floor(Math.random() * fortunes.length)]
+}
